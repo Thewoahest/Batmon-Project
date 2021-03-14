3cb8439d2d8398ee5efd1c0c787928ea5651745c
Phillip <phillipmgonzalez77@gmail.com>
2021-03-13 19:52:51 -0700

    Big update
    Did a whole lot of stuff

---
+++ Batmon/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    public GameObject collectable;
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {  
+        Destroy(collectable, 0);
+    }
+}
---
+++ Batmon/Assets/Scripts/Fly.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {   
+        if (collision.gameObject.tag == "Orange")
+        {
+            Destroy(collision.gameObject);
+            gameManager.addPoints(10);
+        }
+        else if (collision.gameObject.tag == "Boundary")
+        {
+            gameManager.GameOver();
+        }
+        else
+        {
+            Debug.Log("Collsion unhandled at: " + collision.gameObject.name + " With tag: " + collision.gameObject.tag );
+        }
+    }
+}
